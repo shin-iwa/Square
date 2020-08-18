a8afa590ef247ff24f26422bc8fb0f8fd8e4be82
shin-iwa <luckystar0443@gmail.com>
2020-08-18 11:16:28 +0900

    start

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,13 +6,13 @@
 
 @section('content')
 <div class="container">
-  <h1 class='pagetitle'>レビュー詳細ページ</h1>
+  <h1 class='pagetitle'>レビューページ</h1>
   <div class="card">
     <div class="card-body d-flex">
       <section class='review-main'>
-        <h2 class='h2'>本のタイトル</h2>
+        <!-- <h2 class='h2'>本のタイトル</h2> -->
         <p class='h2 mb20'>{{ $review->title }}</p>
-        <h2 class='h2'>レビュー本文</h2>
+        <!-- <h2 class='h2'>レビュー本文</h2> -->
         <p>{{ $review->body }}</p>
       </section>  
       <aside class='review-image'>
